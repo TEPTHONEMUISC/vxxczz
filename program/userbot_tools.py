dbde0434227cf78293ceb99cbd13e39cc9f531e4
vceerw <102238497+vceerw@users.noreply.github.com>
2022-03-23 20:10:34 +0300

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -1,60 +1,64 @@
 import asyncio
-from driver.veez import user
-from pyrogram.types import Message
-from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
+from driver.decorators import authorized_users_only, sudo_users_only, errors
 from driver.filters import command, other_filters
+from driver.veez import user as USER
+from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
-from driver.decorators import authorized_users_only, sudo_users_only
 
 
 @Client.on_message(
-    command(["انضم", f"انضم@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
+    command(["انضم", f"userbotjoin@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
 )
 @authorized_users_only
-async def join_chat(c: Client, m: Message):
-    chat_id = m.chat.id
+@errors
+async def join_group(client, message):
+    chid = message.chat.id
     try:
-        invite_link = await m.chat.export_invite_link()
-        if "+" in invite_link:
-            link_hash = (invite_link.replace("+", "")).split("t.me/")[1]
-            await user.join_chat(f"https://t.me/joinchat/{link_hash}")
-        await m.chat.promote_member(
-            (await user.get_me()).id,
-            can_manage_voice_chats=True
+        invitelink = await client.export_chat_invite_link(chid)
+    except BaseException:
+        await message.reply_text(
+            "• **يخي لا استطيع دعوة حساب المساعد م عندي صلاحية:**\n\n» ❌ __اضافة مستخدمين__",
         )
-        return await user.send_message(chat_id, "✅ userbot entered chat")
+        return
+
+    try:
+        user = await USER.get_me()
+    except BaseException:
+        user.first_name = "music assistant"
+
+    try:
+        await USER.join_chat(invitelink)
     except UserAlreadyParticipant:
-        admin = await m.chat.get_member((await user.get_me()).id)
-        if not admin.can_manage_voice_chats:
-            await m.chat.promote_member(
-                (await user.get_me()).id,
-                can_manage_voice_chats=True
-            )
-            return await user.send_message(chat_id, "✅ userbot already in chat")
-        return await user.send_message(chat_id, "✅ userbot already in chat")
+        pass
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"🛑 م كدر حساب المساعد ينضم 🛑 \n\n**بسبب كثرة الطلبات على الانضمام في المجموعات**"
+            "\n\n**اكتب .انضم او قم باضافتة يدويا**",
+        )
+        return
+    await message.reply_text(
+        f"✅ **تم ياعيني انضم حساب المساعد**",
+    )
 
 
-@Client.on_message(command(["غادر",
-                            f"غادر@{BOT_USERNAME}"]) & filters.group & ~filters.edited
-)
+@Client.on_message(command(["اطلع",
+                            f"leave@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 @authorized_users_only
-async def leave_chat(_, m: Message):
-    chat_id = m.chat.id
+async def leave_one(client, message):
     try:
-        await user.leave_chat(chat_id)
-        return await _.send_message(
-            chat_id,
-            "✅ userbot leaved chat",
-        )
-    except UserNotParticipant:
-        return await _.send_message(
-            chat_id,
-            "❌ userbot already leave chat",
+        await USER.send_message(message.chat.id, "✅ هوه مو صوجكم صوجي اني اجيت باي")
+        await USER.leave_chat(message.chat.id)
+    except BaseException:
+        await message.reply_text(
+            "❌ **ميكدر حساب المساعد يغادر.**\n\n**» اطرده بنفسك ولتصير دودكي**"
         )
 
+        return
 
-@Client.on_message(command(["leaveall", f"leaveall@{BOT_USERNAME}"]))
+
+@Client.on_message(command(["مغادرة كل المجموعات", f"leaveall@{BOT_USERNAME}"]))
 @sudo_users_only
 async def leave_all(client, message):
     if message.from_user.id not in SUDO_USERS:
@@ -62,32 +66,20 @@ async def leave_all(client, message):
 
     left = 0
     failed = 0
-    lol = await message.reply("🔄 **userbot** leaving all chats !")
-    async for dialog in user.iter_dialogs():
+    lol = await message.reply("🔄  **اهلا مطوري** جاري مغادرة كل المجموعات !")
+    async for dialog in USER.iter_dialogs():
         try:
-            await user.leave_chat(dialog.chat.id)
+            await USER.leave_chat(dialog.chat.id)
             left += 1
             await lol.edit(
-                f"Userbot leaving all group...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+                f"عدد المجموعات التي غادرت منها...\n\nLeft: {left} chats.\nFailed: {failed} chats."
             )
         except BaseException:
             failed += 1
             await lol.edit(
-                f"Userbot leaving...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+                f"تم مغادرة جميع المجموعات...\n\nLeft: {left} chats.\nFailed: {failed} chats."
             )
         await asyncio.sleep(0.7)
     await client.send_message(
-        message.chat.id, f"✅ Left from: {left} chats.\n❌ Failed in: {failed} chats."
+        message.chat.id, f"✅ تم مغادرة حساب المساعد من: {left} chats.\n❌ Failed in: {failed} chats."
     )
-
-
-@Client.on_message(filters.left_chat_member)
-async def ubot_leave(c: Client, m: Message):
-    ass_id = (await user.get_me()).id
-    bot_id = (await c.get_me()).id
-    chat_id = m.chat.id
-    left_member = m.left_chat_member
-    if left_member.id == bot_id:
-        await user.leave_chat(chat_id)
-    elif left_member.id == ass_id:
-        await c.leave_chat(chat_id)
